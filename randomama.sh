a6199180a4d8c8c069a7226065a5f49d89de5033
antonio <antonio@architux.com>
2017-07-17 20:27:39 +0200

    add randomama

--- randomama.sh
+++ randomama.sh
@@ -1,7 +1,5 @@
 miam=(" " "mama" "pizza" "pizza" "mama" "japonais" "créperie" "mama" "mama" "japonais") 
 
-
-
 echo "Aujourd'hui tu vas t'étouffer chez (le|la) :"
 for ((i=1 ; i<5 ; i++)) ; do
     result=$(shuf -i 1-9  -n 1)
