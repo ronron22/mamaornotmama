8c929487c6b1ddfff68b2e310119fb160b028621
antonio <antonio@architux.com>
2017-07-18 09:12:03 +0200

    revert

--- randomama.sh
+++ randomama.sh
@@ -1,9 +1,6 @@
-# todo générer une liste de choix unique
+miam=(" " "mama" "pizza" "pizza" "mama" "japonais" "créperie" "mama" "mama" "japonais" "japonais" "mama") 
 
-miam=(" " "mama" "pizza" "pizza" "mama" "japonais" "créperie" "mama" "mama" "japonais") 
 
-echo "Aujourd'hui tu vas t'étouffer chez (le|la) :"
-for ((i=1 ; i<5 ; i++)) ; do
-    result=$(shuf -i 1-9  -n 1)
-    echo "- ${miam[$result]}"
-done
+result=$(shuf -i 1-11 -n 1)
+
+echo "Aujourd'hui tu vas t'étouffer chez (le|la) '${miam[$result]}'"
