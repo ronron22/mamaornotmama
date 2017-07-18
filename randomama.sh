51e5aada586eceb2758415b964e459f26f682b3d
antonio <antonio@architux.com>
2017-07-18 09:39:25 +0200

    revert

--- randomama.sh
+++ randomama.sh
@@ -1,6 +1,6 @@
-miam=(" " "mama" "pizza" "pizza" "mama" "japonais" "créperie" "mama" "mama" "japonais" "japonais" "mama") 
+miam=(" " "mama" "pizza" "mama" "japonais" "créperie" "mama" "mama" "japonais" "japonais" "mama") 
 
 
-result=$(shuf -i 1-11 -n 1)
+result=$(shuf -i 1-10 -n 1)
 
 echo "Aujourd'hui tu vas t'étouffer chez (le|la) '${miam[$result]}'"
